7acfaa8fce351e403cf744823b50dfff53d097b7
victorlico <engvictorsmatos@gmail.com>
2023-01-31 10:15:31 -0300

    aprbs func

--- py_pubsub/aprbs.py
+++ py_pubsub/aprbs.py
@@ -1,15 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-nstep = 300
+nstep = 1000
 
 # random signal generation
 
-a_range = [0, 2]
+a_range = [-1, 1]
 # range for amplitude
 a = np.random.rand(nstep) * (a_range[1]-a_range[0]) + a_range[0]
 
-b_range = [2, 10]
+b_range = [1, 100]
 # range for frequency
 b = np.random.rand(nstep) * (b_range[1]-b_range[0]) + b_range[0]
 b = np.round(b)
@@ -45,7 +45,7 @@ while b[i] < np.size(prbs):
 
 plt.figure(0)
 plt.subplot(2, 1, 1)
-plt.plot(random_signal, drawstyle='steps', label='Random Signal')
+plt.plot(random_signal, drawstyle='steps', label='APRBS')
 plt.legend()
 plt.subplot(2, 1, 2)
 plt.plot(prbs, drawstyle='steps', label='PRBS')
